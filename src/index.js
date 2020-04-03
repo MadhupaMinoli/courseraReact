79de232040528ca77e6f35aa282cef8971f74177
Madhupa Minoli <mmadhupagmail.com>
2020-04-03 13:21:53 +0530

    Component part 1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,141 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Menu from './Components/MenuComponent';
 
+
+// const name = 'Josh Perez';
+// const element = <h1>Hello, {name}</h1>;
+
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const person = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// };
+
+
+
+// function getGreeting(user) {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}!</h1>;
+//   }
+//   return <h1>Hello, Stranger.</h1>;
+// }
+// const element = (
+//   <h1>
+//     {getGreeting()}
+//   </h1>
+// );
+// const element = (
+//   <div>
+//     <h1>Hello!</h1>
+//     <h2>Good to see you here.</h2>
+//   </div>
+// );
+// const title = response.potentiallyMaliciousInput;
+// // This is safe:
+// const element = <h1>{title}</h1>;
+// const element = (
+//   <h1 className="greeting">
+//     Hello, world!
+//   </h1>
+// );
+// // const element = React.createElement(
+// //   'h1',
+// //   {className: 'greeting'},
+// //   'Hello, world!'
+// // );
+// ReactDOM.render(
+//   element,
+//   // <React.StrictMode>
+//   //   <App />
+//   // </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000);
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// const element = <Welcome name="Minoli" />;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// function App1() {
+//   return (
+//     <div>
+//       <Welcome name="Sara" />
+//       <Welcome name="Cahal" />
+//       <Welcome name="Edite" />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <React.StrictMode><App1 /></React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// function formatDate(date) {
+//   return date.toLocaleDateString();
+// }
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <div className="UserInfo">
+//         <img
+//           className="Avatar"
+//           src={props.author.avatarUrl}
+//           alt={props.author.name}
+//         />
+//         <div className="UserInfo-name">
+//           {props.author.name}
+//         </div>
+//       </div>
+//       <div className="Comment-text">{props.text}</div>
+//       <div className="Comment-date">
+//         {formatDate(props.date)}
+//       </div>
+//     </div>
+//   );
+// }
+
+// const comment = {
+//   date: new Date(),
+//   text: 'I hope you enjoy learning React!',
+//   author: {
+//     name: 'Hello Kitty',
+//     avatarUrl: 'https://placekitten.com/g/64/64',
+//   },
+// };
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <App />
+</React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
