fcd462056ff50ea306df974a1c034b2299b56279
Madhupa Minoli <mmadhupagmail.com>
2020-04-16 11:45:45 +0530

    Redux Thunks

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Card, Modal, Row, ModalBody, Label, ModalHeader, Button, CardBody, CardText, CardImg, CardTitle, BreadcrumbItem, Breadcrumb } from 'reactstrap'
 import { Link } from "react-router-dom";
-import { LocalForm, Control,Errors } from 'react-redux-form'
+import { LocalForm, Control, Errors } from 'react-redux-form'
+import { Loading } from './LoadingComponent';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -10,7 +11,7 @@ class CommentForm extends Component {
 
     constructor(props) {
         super(props);
-        
+
 
         this.state = {
             isModalOpen: false
@@ -36,31 +37,31 @@ class CommentForm extends Component {
             <div>
                 <Button outline color="secondary" onClick={this.toggleModal}><span className="fa fa-pencil"></span>Submit Comment</Button>
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                    <ModalHeader  toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
                     <ModalBody>
                         <div className="col-12">
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                            <Row className="form-group">
-                                <Label style={{fontWeight:'bold'}} htmlFor="rating">Rating</Label>
-                                <Control.select model=".rating" name="rating"
-                                    className="form-control">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </Control.select>
-                                
-                            </Row>
-                            <Row className="form-group">
-                                <Label style={{fontWeight:'bold'}} htmlFor="author">Your Name</Label>
-                                <Control.text model=".author" id="author" name="author"
-                                    placeholder="Your Name"
-                                    className="form-control"
-                                    validators={{
-                                         minLength: minLength(3), maxLength: maxLength(15)
-                                    }} />
-                                <Errors
+                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                                <Row className="form-group">
+                                    <Label style={{ fontWeight: 'bold' }} htmlFor="rating">Rating</Label>
+                                    <Control.select model=".rating" name="rating"
+                                        className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+
+                                </Row>
+                                <Row className="form-group">
+                                    <Label style={{ fontWeight: 'bold' }} htmlFor="author">Your Name</Label>
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            minLength: minLength(3), maxLength: maxLength(15)
+                                        }} />
+                                    <Errors
                                         className="text-danger"
                                         model=".author"
                                         show="touched"
@@ -68,24 +69,24 @@ class CommentForm extends Component {
                                             minLength: 'Must be greater than 3 characters',
                                             maxLength: 'Must be 15 characters or less'
                                         }}
-                                     />
-                            </Row>
-                            <Row className="form-group">
-                                <Label style={{fontWeight:'bold'}} htmlFor="comment">Comment</Label>
+                                    />
+                                </Row>
+                                <Row className="form-group">
+                                    <Label style={{ fontWeight: 'bold' }} htmlFor="comment">Comment</Label>
                                     <Control.textarea model=".comment" id="comment" name="comment"
                                         rows="6"
-                                        className="form-control" />    
-                            </Row>
-                            <Row className="form-group">
-                                
+                                        className="form-control" />
+                                </Row>
+                                <Row className="form-group">
+
                                     <Button type="submit" color="primary">
                                         Submit
                                     </Button>
-                                
-                            </Row>
-                        </LocalForm>
+
+                                </Row>
+                            </LocalForm>
                         </div>
-                      
+
                     </ModalBody>
                 </Modal>
             </div>
@@ -145,7 +146,25 @@ function RenderComments({ comments, addComment, dishId }) {
 
 const Dishdetails = (props) => {
 
-    if (props.dish != null) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
@@ -160,9 +179,9 @@ const Dishdetails = (props) => {
                 </div>
                 <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} 
-                    addComment={props.addComment}
-                   dishId={props.dish.id}/>
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id} />
                 </div>
             </div>
         );
